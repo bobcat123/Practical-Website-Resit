31306a992ac372a10650301b2d956bddea15c5d5
bobcat123 <m.stuart1990@gmail.com>
2022-07-19 21:00:57 +0100

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    loginButton = document.getElementById("loginButton")
+    loginButton.addEventListener("click", ()=>{
+        alert("Login not available at the moment")
+    })
+})
